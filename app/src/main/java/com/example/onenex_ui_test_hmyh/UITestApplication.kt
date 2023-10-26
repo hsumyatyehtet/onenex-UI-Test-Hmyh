aa7c08c20fccab9f16eb56d54f43b392dfc6ae78
Hsu Myat Ye Htet <hsumyatyehtet@gmail.com>
2023-10-27 00:16:51 +0630

    application add

---
+++ app/src/main/java/com/example/onenex_ui_test_hmyh/UITestApplication.kt
@@ -0,0 +1,10 @@
+package com.example.onenex_ui_test_hmyh
+
+import android.app.Application
+
+class UITestApplication: Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+    }
+}
